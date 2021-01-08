7962c6fa0418bbf9bdb0757b155268ba914acd4d
Aaron Kessler <kessler.aaron@gmail.com>
2021-01-07 22:25:15 -0500

    moved raw data file to inst/extdata and changed reference in data-raw R script

--- data-raw/precinctlayout.R
+++ data-raw/precinctlayout.R
@@ -5,7 +5,11 @@ usethis::use_data(precinctlayout, overwrite = TRUE)
 
 #read in one of the races, in this case presidential from Saratoga County, NY
 #this will show the format the data should be in before beginning this process
-precinctsampledata_ny <- readxl::read_excel("data-raw/Saratoga_NY_GE20_cleaned.xlsx",
+sample_prez <- system.file("extdata",
+                           "Saratoga_NY_GE20_cleaned.xlsx",
+                           package = "precinctsopenelex")
+
+precinctsampledata_ny <- readxl::read_excel(sample_prez,
                                     sheet = "presidential")
 
 
