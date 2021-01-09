4006952fd9611b9d8ef3d0ecbee793779619949a
Aaron Kessler <kessler.aaron@gmail.com>
2021-01-08 23:14:16 -0500

    added more descriptive documentation to sample data fields

--- R/data.R
+++ R/data.R
@@ -1,20 +1,21 @@
 #' Precinct results from 2020 presidential election in Saratoga County, NY.
 #'
-#' A dataset containing precinct-by-precinct election results. Note that NY rules allow
-#' candidates to be listed under more than one party on the ballot.
+#' A dataset containing precinct-by-precinct election results. Note that NY's odd rules allow
+#' candidates to be listed under more than one party on the ballot, which is why you'll
+#' see Trump in there twice under both the Republican and Conservative parties.
 #'
 #' @format A data frame with 196 rows and 11 variables:
 #' \describe{
 #'   \item{precinct}{election precinct name}
-#'   \item{'Joseph R. Biden - DEM'}{number of votes}
-#'   \item{'Donald J. Trump - REP'}{number of votes}
-#'   \item{'Donald J. Trump - CON'}{number of votes}
-#'   \item{'Howie Hawkins - GRE'}{number of votes}
-#'   \item{'Jo Jorgensen - LIB'}{number of votes}
-#'   \item{'Brock Pierce - IND'}{number of votes}
-#'   \item{WriteIn}{number of write in votes}
-#'   \item{Blanks}{number of undervotes, where voter skipped race}
-#'   \item{Voids}{number of overvotes, where voter marked multiple candidates}
+#'   \item{'Joseph R. Biden - DEM'}{votes for Biden as Democrat}
+#'   \item{'Donald J. Trump - REP'}{votes for Trump as Republican}
+#'   \item{'Donald J. Trump - CON'}{votes for Trump as Conservative Party}
+#'   \item{'Howie Hawkins - GRE'}{votes for Hawkins as Green Party}
+#'   \item{'Jo Jorgensen - LIB'}{votes for Jogensen as Libertartian}
+#'   \item{'Brock Pierce - IND'}{boes for Pierce as Independence Party}
+#'   \item{WriteIn}{votes for write-in candidates}
+#'   \item{Blanks}{undervotes, where voter skipped the race and left it blank}
+#'   \item{Voids}{overvotes, where voter improperly marked multiple candidates}
 #'   ...
 #' }
 #' @source \url{https://www.saratogacountyny.gov/2020-official-general-election-results/}
