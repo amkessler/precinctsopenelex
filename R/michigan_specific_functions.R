206cc51a9b228c3a2924a084ce24410702473f7a
Kessler <ak113714@bna.com>
2021-01-15 00:12:37 -0500

    replace former MI functions with a single one for dynamic column formatting

--- R/michigan_specific_functions.R
+++ R/michigan_specific_functions.R
@@ -1,97 +1,29 @@
-#' Michigan-Specific Function to Rename Columns for Straight-Party Data
+#' Michigan-Specific Function to Rename Columns in Correct Format
 #'
-#' This function takes a dataframe containing precinct data with the original Michigan headers for straight-party
-#' vote totals and converts the headers to proper formatting to use with package's processing functions. It is tailored
-#' to work with the way Michigan's counties list their candidates/parties and so will only with with that state's results.
-#'
-#' @param df
-#'
-#' @return df with renamed columns
-#' @export
-#'
-#' @examples \dontrun{
-#' mi_replace_colnames_straightparty(mydataframe)
-#' }
-mi_replace_colnames_straightparty <- function(df) {
-  newcolnames <- c('Democratic Party - DEM' = 'democratic_party_dem',
-                   'Republican Party - REP' = 'republican_party_rep',
-                   'Libertarian Party - LIB' = 'libertarian_party_lib',
-                   'U.S. Taxpayers Party - UST' = 'u_s_taxpayers_party_ust',
-                   'Working Class Party - WCP' = 'working_class_party_wcp',
-                   'Green Party - GRN' = 'green_party_grn',
-                   'Natural Law Party - NLP' = 'natural_law_party_nat',
-                   'WriteIn' = 'unresolved_write_in')
-
-  df_renamed <- df %>%
-    janitor::clean_names() %>%
-    rename(!!!newcolnames)
-
-  return(df_renamed)
-
-}
-
-
-
-#' Michigan Specific Function to Rename Columns for Presidential Data
-#'
-#' This function takes a dataframe containing precinct data with the original Michigan headers for presidential
-#' election results and converts the headers to proper formatting to use with package's processing functions. It is tailored
-#' to work with the way Michigan's counties list their candidates/parties and so will only with with that state's results.
+#' This function takes a dataframe containing precinct data with the original Michigan candidate/party headers and
+#' converts the headers to proper formatting to use with package's processing functions. It is tailored
+#' to work with the way Michigan counties' format of "Candidate (PARTY)" and also incorporates step to remove empty columns.
 #'
 #' @param df
 #'
-#' @return df with renamed columns
+#' @return df with renamed columns and completely empty columns removed
 #' @export
 #'
 #' @examples \dontrun{
-#' mi_replace_colnames_presidential(mydataframe)
+#' mi_format_column_names(mydataframe)
 #' }
-mi_replace_colnames_presidential <- function(df) {
-  newcolnames <- c('Joseph R. Biden - DEM' = 'joseph_r_biden_kamala_d_harris_dem',
-                   'Donald J. Trump - REP' = 'donald_j_trump_michael_r_pence_rep',
-                   'Jo Jorgensen - LIB' = 'jo_jorgensen_jeremy_cohen_lib',
-                   'Don Blankenship - UST' = 'don_blankenship_willia_m_mohr_ust',
-                   'Howie Hawkins - GRN' = 'howie_hawkins_angela_walker_grn',
-                   'Rocky De La Fuente - NAT' = 'rocky_de_la_fuente_darcy_richardson_nat',
-                   'WriteIn' = 'unresolved_write_in')
+mi_format_column_names <- function(df) {
 
-  df_renamed <- df %>%
-    janitor::clean_names() %>%
-    rename(!!!newcolnames)
+  df <- df %>%
+    janitor::remove_empty("cols")
 
-  return(df_renamed)
+  newcolnames <- df %>%
+    names() %>%
+    str_squish() %>%
+    str_replace_all("\\(", "- ") %>%
+    str_remove_all("\\)")
 
+  colnames(df) <- newcolnames
+
+  return(df)
 }
-
-
-#' Michigan Specific Function to Rename Columns for US Senate Data
-#'
-#' This function takes a dataframe containing precinct data with the original Michigan headers for US Senate
-#' election results and converts the headers to proper formatting to use with package's processing functions. It is tailored
-#' to work with the way Michigan's counties list their candidates/parties and so will only with with that state's results.
-#'
-#' @param df
-#'
-#' @return df with renamed columns
-#' @export
-#'
-#' @examples \dontrun{
-#' mi_replace_colnames_ussenate(mydataframe)
-#' }
-mi_replace_colnames_ussenate <- function(df) {
-  newcolnames <- c('Gary Peters - DEM' = 'gary_peters_dem',
-                   'John James - REP' = 'john_james_rep',
-                   'Valerie L. Willis - UST' = 'valerie_l_willis_ust',
-                   'Marcia Squier - GRN' = 'marcia_squier_grn',
-                   'Doug Dern - NAT' = 'doug_dern_nat',
-                   'WriteIn' = 'unresolved_write_in')
-
-  df_renamed <- df %>%
-    janitor::clean_names() %>%
-    rename(!!!newcolnames)
-
-  return(df_renamed)
-
-}
-
-
